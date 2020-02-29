d25704d9d3edbe4bea3139534c41985fb96f650b
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-02-29 15:12:46 -0800

    k

--- hellohandlebars.js
+++ hellohandlebars.js
@@ -2,49 +2,56 @@ var express = require('express');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 7034);
 
 app.get('/',function(req,res){
-  res.render('home.handlebars') //We can omit the .handlebars extension as we do below
+  res.render('home');
 });
 
-
 app.get('/show-data',function(req,res){
-    var context = {};
-    context.sentData = req.query.myData;
-    res.render('show-data', context);
-  });
+  var context = {};
+  context.sentData = req.query.myData;
+  res.render('show-data', context);
+});
 
-
-  app.get('/get-loopback',function(req,res){
-    var qParams = "";
-    for (var p in req.query){
-      qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
-    }
-    qParams = qParams.substring(0,qParams.lastIndexOf(','));
-    qParams += '.';
-    var context = {};
-    context.dataList = qParams;
-    res.render('get-loopback', context);
-  });
-
+app.get('/get-loopback',function(req,res){
+  var qParams = "";
+  for (var p in req.query){
+    qParams += "The name " + p + " contains the value " + req.query[p] + ", ";
+  }
+  qParams = qParams.substring(0,qParams.lastIndexOf(','));
+  qParams += '.';
+  var context = {};
+  context.dataList = qParams;
+  res.render('get-loopback', context);
+});
 
 app.get('/get-loopback-improved',function(req,res){
-    var qParams = [];
-    for (var p in req.query){
-      qParams.push({'name':p,'value':req.query[p]})
-    }
-    var context = {};
-    context.dataList = qParams;
-    res.render('get-loopback-improved', context);
-  });
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render('get-loopback-improved', context);
+});
 
-
-app.get('/other-page',function(req,res){
-  res.render('other-page');
+app.post('/post-loopback', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post-loopback', context);
 });
 
 app.use(function(req,res){
