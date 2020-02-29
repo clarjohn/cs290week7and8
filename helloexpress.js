b948c608b1e90788ef6330a95cb88d9c24abc44a
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-02-29 12:29:40 -0800

    Create helloexpress.js

---
+++ helloexpress.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', 3000);
+
+app.get('/', function(reg,res)){
+    res.type('text/plain');
+    res.send('welcome to the main page!!!');
+}
+
+app.get('/other-page', function(reg,res){
+    res.type('text/plain');
+    res.send('Yes this is working');
+})
+
+app.use(function(reg,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404-Its all gone to hell.')
+
+});
+
+app.use(function(err,reg,res, next){
+        res.type('plain/text');
+        res.status(500);
+        res.send('500 - this is the server error');
+
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port')+', press Ctrl-c to terminate.')
+})
